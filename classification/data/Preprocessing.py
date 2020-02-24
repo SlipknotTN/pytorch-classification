d9eca60ca391016da605b3b2f2cd11fa6275564e
SlipknotTN <micheletoni87@tin.it>
2020-02-24 18:09:46 +0100

    Fixed resize and crops

--- classification/data/Preprocessing.py
+++ classification/data/Preprocessing.py
@@ -16,14 +16,14 @@ class Preprocessing(object):
             # Resize smaller dimension to 256 and then random crop 224x224.
             # ToTensor converts HWC PIL.Image to CHW float tensor.
             self.data_transform_train = transforms.Compose([
-                transforms.Resize(config.train_resize),
-                transforms.RandomCrop(config.input_size),
+                transforms.Resize((config.train_resize, config.train_resize)),
+                transforms.RandomCrop((config.input_size, config.input_size)),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ])
 
             self.data_transform_val = transforms.Compose([
-                transforms.Resize((config.input_size)),
+                transforms.Resize((config.input_size, config.input_size)),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ])
